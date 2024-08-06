43d5d160232b9a91144d9ec5747c82c565eaffeb
Injun817 <choinjun070817@naver.com>
2024-08-06 04:54:36 +0000

    init

--- app.py
+++ app.py
@@ -1,20 +1,49 @@
-from flask import Flask, request, redirect, render_template, url_for, session
+from flask import Flask, request, redirect, render_template, url_for, session, jsonify
 from modules.api import api
+from env import ENUM
+
+
 app = Flask(__name__)
 
 interpreter = api()
+app.secret_key = ENUM.APP_KEY
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/recognize', methods=['POST'])
-def button():
-    message = "테스트"
-    code = "01"
-    tmp = interpreter.language(message, code)
-    print(tmp)
-    return render_template('index.html',tmp = tmp)
+@app.route('/process_text', methods=['POST'])
+def process_text():
+    data = request.get_json()
+    text = data.get('text')
+    
+    processed_text = text.upper() 
+
+    
+
+    print(processed_text)
+    if (processed_text == "영어") :
+        language_result = interpreter.language(processed_text,'01')
+    elif (processed_text == "스페인어") :
+        language_result = interpreter.language(processed_text,'02')
+    elif (processed_text == "한국어") :
+        language_result = interpreter.language(processed_text,'03')
+    
+    sentence_result = processed_text
+    
+
+
+    print(sentence_result)
+
+    result_text = getattr(sentence_result, 'text', str(language_result))
+
+    session['english_result'] = result_text
+
+    print(result_text)
+
+    return jsonify({'result': result_text})
+
     
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True, port=8080)
+    app.run('0.0.0.0', debug=True, port=ENUM.PORT)
